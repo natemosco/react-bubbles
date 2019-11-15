cee6fc24a215a8fd6bf8f4048a690cfac5f72ed3
Nathaniel Mosco <natemosco@gmail.com>
2019-11-15 11:20:23 -0500

    testing if GUI will add changes to login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,11 @@
 import React from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  axiosWithAuth()
+    .get
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
